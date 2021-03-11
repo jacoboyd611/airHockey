9a5afb22d8b090992001fcffb1fa48519762f533
Jacob Boyd <jacoboyd611@outlook.com>
2021-03-11 10:26:29 -0500

    finished

--- airHockey/Form1.Designer.cs
+++ airHockey/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimerTick = new System.Windows.Forms.Timer(this.components);
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimerTick
@@ -38,11 +42,59 @@
             this.gameTimerTick.Interval = 20;
             this.gameTimerTick.Tick += new System.EventHandler(this.GameTimerTick_Tick);
             // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.Location = new System.Drawing.Point(373, 35);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(18, 19);
+            this.p1ScoreLabel.TabIndex = 0;
+            this.p1ScoreLabel.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(392, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ":";
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.Location = new System.Drawing.Point(408, 35);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(18, 19);
+            this.p2ScoreLabel.TabIndex = 2;
+            this.p2ScoreLabel.Text = "0";
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.winnerLabel.Font = new System.Drawing.Font("mono 07_65", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLabel.ForeColor = System.Drawing.Color.Red;
+            this.winnerLabel.Location = new System.Drawing.Point(312, 173);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(185, 64);
+            this.winnerLabel.TabIndex = 3;
+            this.winnerLabel.Text = "P1 WINS";
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winnerLabel.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(834, 450);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p1ScoreLabel);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -50,12 +102,17 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer gameTimerTick;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Label winnerLabel;
     }
 }
 
--- airHockey/Form1.cs
+++ airHockey/Form1.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿/// Jacob Boyd
+/// Mr T.
+/// ICS3U
+/// 2D air hockey game
+
+using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Media;
 
 namespace airHockey
 {
@@ -13,7 +19,8 @@ namespace airHockey
         }
         #region Variables
         int height = 371;
-
+        int p1Score = 0;
+        int p2Score = 0;
 
         int paddle1X = 150;
         int paddle1Y = 150;
@@ -27,14 +34,12 @@ namespace airHockey
         int paddleHeight = 25;
         int paddleSpeed = 7;
 
-        int ballX = 400;
-        int ballY = 195;
+        int ballX = 300;
+        int ballY = 220;
         int ballXSpeed = 4;
         int ballYSpeed = 4;
         int ballWidth = 10;
         int ballHeight = 10;
-
-
 
         bool wDown = false;
         bool sDown = false;
@@ -47,15 +52,20 @@ namespace airHockey
         bool leftArrowDown = false;
         bool rightArrowDown = false;
 
+        bool gameStart = false;
+
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush redBrush = new SolidBrush(Color.Red);
         SolidBrush blackBrush = new SolidBrush(Color.Black);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
 
+        SoundPlayer hitNoise = new SoundPlayer(Properties.Resources.hitNoise);
+        SoundPlayer airHorn = new SoundPlayer(Properties.Resources.airHorn);
         #endregion
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            // Detects key presses
             switch (e.KeyCode)
             {
                 case Keys.W:
@@ -89,6 +99,7 @@ namespace airHockey
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            // detect when the button is released
             switch (e.KeyCode)
             {
                 case Keys.W:
@@ -121,9 +132,13 @@ namespace airHockey
 
         private void GameTimerTick_Tick(object sender, EventArgs e)
         {
-            //move ball 
-            ballX += ballXSpeed;
-            ballY += ballYSpeed;
+            //move ball           
+            if (gameStart == false) { }
+            else
+            {
+                ballX += ballXSpeed;
+                ballY += ballYSpeed;
+            }
 
             #region move players
             if (wDown == true && paddle1Y > 90)
@@ -172,15 +187,15 @@ namespace airHockey
             if (ballY < 90 || ballY > height - ballHeight)
             {
                 ballYSpeed *= -1;
+                hitNoise.Play();
             }
 
-
             #region Hitboxes
-            Rectangle player1Rec = new Rectangle(paddle1X, paddle1Y, paddleWidth, paddleHeight);
-            
+            Rectangle player1Rec = new Rectangle(paddle1X, paddle1Y + 5, paddleWidth, paddleHeight - 10); 
+            Rectangle player1RecTopBottom = new Rectangle(paddle1X + 5, paddle1Y, paddleWidth - 10, paddleHeight);
 
-            Rectangle player2Rec = new Rectangle(paddle2X, paddle2Y, paddleWidth, paddleHeight);
-            
+            Rectangle player2Rec = new Rectangle(paddle2X, paddle2Y + 5, paddleWidth, paddleHeight - 10);
+            Rectangle player2RecTopBottom = new Rectangle(paddle2X + 5, paddle2Y, paddleWidth - 10, paddleHeight);
 
             Rectangle ballRec = new Rectangle(ballX, ballY, ballWidth, ballHeight);
 
@@ -195,36 +210,147 @@ namespace airHockey
 
             #endregion
 
+            #region wall detection
             //check if ball hits the side walls
-            if (farWallTop.IntersectsWith(ballRec)) { ballXSpeed *= -1; }
-            else if (farWallBottom.IntersectsWith(ballRec)) { ballXSpeed *= -1; }
-            else if (closeWallTop.IntersectsWith(ballRec)) { ballXSpeed *= -1; }
-            else if (closeWallBottom.IntersectsWith(ballRec)) { ballXSpeed *= -1; }
+            if (farWallTop.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                hitNoise.Play();
+            }
+            else if (farWallBottom.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                hitNoise.Play();
+            }
+            else if (closeWallTop.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                hitNoise.Play();
+            }
+            else if (closeWallBottom.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                hitNoise.Play();
+            }
+            #endregion
 
             #region player detection
 
+            if (player1Rec.IntersectsWith(ballRec))
+            {
+                gameStart = true;
+                if (ballX < paddle1X)
+                {
+                    ballXSpeed *= -1;
+                    ballX = paddle1X - paddleWidth - 1;
+                    hitNoise.Play();
+                }
+                else if (ballX > paddle1X)
+                {
+                    ballXSpeed *= -1;
+                    ballX = paddle1X + paddleWidth + 1;
+                    hitNoise.Play();
+                }
+            }
+
+            if (player1RecTopBottom.IntersectsWith(ballRec))
+            {
+                gameStart = true;
+                if (ballY < paddle1Y)
+                {
+                    ballYSpeed *= -1;
+                    ballY = paddle1Y - paddleHeight - 1;
+                    hitNoise.Play();
+                }
+                else if (ballY > paddle1Y)
+                {
+                    ballYSpeed *= -1;
+                    ballY = paddle1Y + paddleHeight + 1;
+                    hitNoise.Play();
+                }
+            }
+
+            if (player2Rec.IntersectsWith(ballRec))
+            {
+                gameStart = true;
+                if (ballX < paddle1X)
+                {
+                    ballXSpeed *= -1;
+                    ballX = paddle2X - paddleWidth - 1;
+                    hitNoise.Play();
+                }
+                else if (ballX > paddle2X)
+                {
+                    ballXSpeed *= -1;
+                    ballX = paddle2X + paddleWidth + 1;
+                    hitNoise.Play();
+                }
+            }
+
+            if (player2RecTopBottom.IntersectsWith(ballRec))
+            {
+                gameStart = true;
+                if (ballY < paddle2Y)
+                {
+                    ballYSpeed *= -1;
+                    ballY = paddle2Y - paddleHeight - 1;
+                    hitNoise.Play();
+                }
+                else if (ballY > paddle2Y)
+                {
+                    ballYSpeed *= -1;
+                    ballY = paddle2Y + paddleHeight + 1;
+                    hitNoise.Play();
+                }
+            }
             #endregion
 
+            #region goal and output
             //Check for goal
             if (p1Goal.IntersectsWith(ballRec))
             {
-                ballX = 400;
-                ballY = 195;
+                gameStart = false;
+                p2Score++;
+                ballX = 300;
+                ballY = 220;
+                airHorn.Play();
             }
 
             if (p2Goal.IntersectsWith(ballRec))
             {
-                ballX = 400;
-                ballY = 195;
+                gameStart = false;
+                p1Score++;
+                ballX = 500;
+                ballY = 220;
+                airHorn.Play();
             }
+            p1ScoreLabel.Text = $"{p1Score}";
+            p2ScoreLabel.Text = $"{p2Score}";
+            if(p1Score == 3)
+            {
+                winnerLabel.Visible = true;
+                winnerLabel.ForeColor = Color.DodgerBlue;
+                winnerLabel.Text = "P1 WINS";
+                gameTimerTick.Enabled = false;
+            }
+            if (p2Score == 3)
+            {
+                winnerLabel.Visible = true;
+                winnerLabel.ForeColor = Color.Red;
+                winnerLabel.Text = "P2 WINS";
+                gameTimerTick.Enabled = false;
+            }
+            #endregion
             Refresh();
         }
         
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //white background and center line
             e.Graphics.FillRectangle(whiteBrush, 100, 75, 605, 300);
             e.Graphics.FillRectangle(redBrush, 400, 75, 5, 300);
 
+            //borders
             e.Graphics.FillRectangle(blackBrush, 100, 75, 600, 5);
             e.Graphics.FillRectangle(blackBrush, 100, 375, 600, 5);
             e.Graphics.FillRectangle(blackBrush, 700, 75, 5, 110);
@@ -232,16 +358,16 @@ namespace airHockey
             e.Graphics.FillRectangle(blackBrush, 100, 75, 5, 110);
             e.Graphics.FillRectangle(blackBrush, 100, 265, 5, 115);
 
+            //nets
             e.Graphics.FillRectangle(redBrush, 85, 185, 15, 80);
             e.Graphics.FillRectangle(redBrush, 705, 185, 15, 80);
 
+            //ball
             e.Graphics.FillRectangle(blackBrush, ballX, ballY, ballWidth, ballHeight);
 
+            //paddles
             e.Graphics.FillRectangle(blueBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
             e.Graphics.FillRectangle(redBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
-
-
-
         }
     }
 }
